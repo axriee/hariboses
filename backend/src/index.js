0823667f893b6135ebb8503e24ab6553e8b2b77c
aixirtt <ttmagpatoc2021@plm.edu.ph>
2025-12-21 19:50:20 +0800

    startServer func added

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,21 @@ import express from 'express';
 import { ENV } from './lib/env.js';
 import { connectDB } from './lib/db.js';
 
-await connectDB();
-
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.listen(ENV.PORT, () => console.log(`Server is running on port ${ENV.PORT}`)); 
+const startServer = async () => {   
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => console.log(`Server is running on port ${ENV.PORT}`));
+    }
+    catch (error) {
+        console.error('Failed to start server:', error);
+        process.exit(1);
+    }
+};
+
+startServer();
