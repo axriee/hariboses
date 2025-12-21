95103d7105aa393fbee2b3b0bf5caf19e563441a
aixirtt <ttmagpatoc2021@plm.edu.ph>
2025-12-21 19:34:54 +0800

    db models created

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import { ENV } from './lib/env.js';
+import { connectDB } from './lib/db.js';
+
+await connectDB();
 
 const app = express();
 
-app.listen(5001, () => {
-  console.log('Server is running on port 5001');
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
+
+app.listen(ENV.PORT, () => console.log(`Server is running on port ${ENV.PORT}`)); 
---
+++ backend/src/models/Comment.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    // KEY FOR REPLIES: Points to the comment being replied to
+    parentComment: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment',
+        default: null
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    isAdminReply: {
+        type: Boolean,
+        default: false
+    }
+  },
+  { timestamps: true }
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+export default Comment;
