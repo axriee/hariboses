2313d1c395a853e0aaff5bce1726894cc2250dd9
axriee <152049754+axriee@users.noreply.github.com>
2026-01-10 19:45:57 +0800

    Revert "rn setup / blank slate"

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,8 +1,23 @@
 import { Stack } from "expo-router";
 import "../global.css";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider 
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+      tokenCache={tokenCache}
+    >
+      <Stack >
+        <Stack.Screen
+          name="(auth)"
+          options={{ headerShown: false }}
+        />
+      </Stack>
+    </ClerkProvider>
+  )
+
 }
 
 
